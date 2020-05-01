e0543f61ac9030a41eeb6ac79dd1fb9990296766
alpha bah <alphaousbah@gmail.com>
2020-05-01 19:18:07 +0200

    creation of cycle.py

---
+++ cycle.py
@@ -0,0 +1,13 @@
+class Cycle(*args):
+
+    """ defines the class Cycle """
+    
+    # constructor of Cycle
+    def __init__(self, model):
+        self.model = model
+    
+    # methods of Cycle
+    
+    # display feature
+    def display(self):
+        print("model:", self.model)
